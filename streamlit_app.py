df80ce36273232d2fb8d60df63ac27281456d94a
kos1nys <mrshark854@gmail.com>
2025-04-16 03:40:01 +0300

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from st_pages import add_page_title, get_nav_from_toml
 st.set_page_config(layout="wide")
 nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
-st.logo("logo.png")
+# st.logo("logo.png")
 
 pg = st.navigation(nav)
 
